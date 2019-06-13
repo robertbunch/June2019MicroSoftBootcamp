8501175ae005aabdc56b3273ff1e7a7de89c43c4
Robert Bunch <rdbunch@gmail.com>
2019-06-13 10:58:42 -0700

    Add Logic App code

---
+++ SensorMonitorApplication/SensorMonitorApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+
+namespace SensorMonitorApplication
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {
+            SendPost();
+            Console.ReadKey();
+        }
+        public async static void SendPost()
+        {
+            string myJson = "{'sid':3,'value':97,'type':'Temperature'}";
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(
+                "https://prod-126.westus.logic.azure.com:443/workflows/0c5938c46aff41cfba3ef238690daa33/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=LQqhoOEuidodO5hYqo6lOvOlcI_sE2r1tRcu5pFqwl4",
+                new StringContent(myJson, Encoding.UTF8, "application/json"));
+                Console.WriteLine(response.StatusCode);
+            }
+        }
+    }
+}
