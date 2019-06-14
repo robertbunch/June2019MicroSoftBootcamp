1bbadffe2c86d67698ee856f364663c5d724a082
Robert Bunch <rdbunch@gmail.com>
2019-06-14 10:01:17 -0700

    Add Sensormontorit

--- TextRPG/TextRPG/Hero.cs
+++ TextRPG/TextRPG/Hero.cs
@@ -6,9 +6,17 @@ namespace TextRPG
 {
     public class Hero
     {
+        public delegate void HeroAction(Hero hero);
+        public static void Attack()
+        {
+
+        }
+
         public string Name { get; set; }
         public string SuperPower { get; set; }
         public List<string> HeroInventory { get; set; }
         public string SecretIdentity { get; set; }
+
+        public Action takeAction = Attack;
     }
 }
